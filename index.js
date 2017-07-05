5b4b0a1c1d9008065b8b976c7a476d11fcec8f36
Hristo Iliev <hsiliev@gmail.com>
2017-07-05 10:45:15 +0300

    Add Mongo write/read endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const cfenv = require('cfenv');
 const appEnv = cfenv.getAppEnv();
@@ -10,11 +11,45 @@ const url = appEnv.getServiceURL('mymongo', {
   auth: ['username', 'password']
 });
 
+const MongoClient = require('mongodb').MongoClient;
+const extend = require('lodash').extend;
+
 app.get('/', (req, res) => {
   res.send(`Hello with service ${url}`);
+});
+
+app.use(bodyParser.json());
+
+app.post('/items/:itemName', (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err)
+      return res.status(500).send(err);
+
+    const collection = db.collection('documents');
+    collection.insertOne(extend(req.body, {
+      _id: req.params.itemName
+    }), (err, result) => {
+      if (err)
+        return res.status(500).send(err);
+      res.status(201).location(`/items/${req.params.itemName}`).send(result);
+      db.close();
+    });
+  });
+});
+
+app.get('/items/:itemName', (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err)
+      return res.status(500).send(err);
+
+    const collection = db.collection('documents');
+    collection.findOne({_id: req.params.itemName}, (err, result) => {
+      res.send(result);
+      db.close();
+    });
+  })
 });
 
 app.listen(appEnv.port, appEnv.bind, () => {
   console.log(`Sample app listening on ${appEnv.url}`);
 });
-
